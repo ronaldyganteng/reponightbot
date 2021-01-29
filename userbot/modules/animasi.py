c2969fa4170c1abbe99ffb1bd367553c2a1bd8c1
Irham <74205184+IrhamFadzillah@users.noreply.github.com>
2021-01-30 04:38:02 +0700

    Update animasi.py

--- userbot/modules/animasi.py
+++ userbot/modules/animasi.py
@@ -302,7 +302,7 @@ async def typewriter(typew):
 
 
 CMD_HELP.update({
-    "vip":
+    "animasi":
     "`.bulan` ; `.hati` ; `.bernyanyi`\
     \nUsage: liat aja.\
     \n\n`.helikopter` ; `.tank` ; `.tembak`\n`.bundir`\
