27e3b174e8b3b13d20b7041660767057ce09626d
Irham <74205184+IrhamFadzillah@users.noreply.github.com>
2021-01-29 20:07:44 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -71,7 +71,7 @@ async def killdabot(event):
 async def reedme(e):
     await e.edit(
         "Here's something for you to read:\n"
-        "\n[WeebProject's README.md file](https://github.com/BianSepang/WeebProject/blob/master/README.md)"
+        "\n[WeebProject's README.md file](https://github.com/IrhamFadzillah/NightCore/blob/master/README.md)"
         "\n[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-02)"
         "\n[Setup Guide - Google Drive](https://telegra.ph/How-To-Setup-Google-Drive-04-03)"
         "\n[Setup Guide - LastFM Module](https://telegra.ph/How-to-set-up-LastFM-module-for-Paperplane-userbot-11-02)"
@@ -98,7 +98,7 @@ async def repeat(rep):
 @register(outgoing=True, pattern=r"^\.repo$")
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
-    await wannasee.edit("Here is my [Repo](https://github.com/BianSepang/WeebProject)")
+    await wannasee.edit("[NightCore](https://github.com/IrhamFadzillah/NightCore)")
 
 
 @register(outgoing=True, pattern=r"^\.raw$")
